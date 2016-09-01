42b096023e4201eb7e15d249f702e656ed734064
edezengrini <edezengrini@gmail.com>
2016-09-01 17:06:58 -0300

    Alteração 01/09/2016

--- src/common/base/FlagBase.java
+++ src/common/base/FlagBase.java
@@ -11,12 +11,19 @@ public class FlagBase implements Serializable {
 	public static final Long PRODUTO_IDFLAGSTATUS_ATIVO = new Long(1);
 	public static final Long PRODUTO_IDFLAGSTATUS_BAIXADO = new Long(2);
 
+    public static final Long PESSOA_IDFLAGSTATUS_ATIVO = new Long(3);
+    public static final Long PESSOA_IDFLAGSTATUS_INATIVO = new Long(4);
+	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public List getListOfFlagBean() {
 		List list = new ArrayList();
 
 		list.add(new FlagBean(1, "Ativo", "PRODUTO", "IDFLAGTIPOPRODUTO"));
 		list.add(new FlagBean(2, "Baixado", "PRODUTO", "IDFLAGTIPOPRODUTO"));
+
+        list.add(new FlagBean(3, "Ativo", "PESSOA", "IDFLAGPESSOAATIVA"));
+        list.add(new FlagBean(4, "Inativo", "PESSOA", "IDFLAGPESSOAINATIVA"));
+		
 		return list;
 	}
 }
